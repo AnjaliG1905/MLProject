0cefe82214abbf7a0455cae1fe7a53869f302844
Anjali Gaonkar <anjalikop4@gmail.com>
2025-07-06 21:26:58 +0530

    application.run correction

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
 
-app=application
+application=application
 
 ## Route for a home page
 
@@ -42,4 +42,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
