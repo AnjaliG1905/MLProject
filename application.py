3bc4d2dbaf885c8f9c525af2e101faa2bec64ac9
Anjali Gaonkar <anjalikop4@gmail.com>
2025-07-06 21:43:13 +0530

    fix: setup Procfile, requirements, and corrected application run config

--- application.py
+++ application.py
@@ -7,8 +7,6 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
 
-application=application
-
 ## Route for a home page
 
 @application.route('/')
@@ -41,5 +39,3 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-if __name__=="__main__":
-    application.run(host="0.0.0.0")
